562a77bf2d411430f6f90263de4dd0aaa3fc1406
Shompa <140482017+shompaakter@users.noreply.github.com>
2026-06-09 16:07:18 +0600

    Fix substring frequency counting in Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,19 @@
+text=input()
+k=int(input())
+freq={}
+for i in range(len(text)  - k+1):
+    p = (text[i:i+k])
+    freq[p]=freq.get(p ,0)+1
 
+
+max_c = max(freq.values())
+
+result=[]
+
+
+for j in freq:
+
+        if freq[j]==max_c:
+            result.append(j)
+
+print(" ".join(sorted(result)))
