0043d09d9de30e1cb3e32e741ce12ae1f62b4e3c
Shompa <140482017+shompaakter@users.noreply.github.com>
2026-06-16 16:31:44 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,14 @@
+pattern = input()
+text = input()
+c = int(input())
 
+for i in range(len(text) - len(pattern) + 1):
+    sub_str = text[i:i + len(pattern)]
+    count = 0
+
+    for j in range(len(pattern)):
+        if pattern[j] != sub_str[j]:
+            count += 1
+
+    if count <= c:
+        print(i)
